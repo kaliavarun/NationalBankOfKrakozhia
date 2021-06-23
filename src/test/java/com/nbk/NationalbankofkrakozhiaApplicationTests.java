9aafd129d22f7a6374f12e39587be96c99cbf18d
varun <arian.veron@gmail.com>
2021-06-24 00:34:00 +0200

    first draft

---
+++ src/test/java/com/nbk/NationalbankofkrakozhiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalbankofkrakozhiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
